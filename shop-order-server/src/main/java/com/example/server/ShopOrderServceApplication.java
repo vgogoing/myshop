6a9eae014201e0601600c6fd27b23618b3a122ef
魏翔 <vgogoing@gmail.com>
2024-02-25 21:14:39 +0800

    单机nacos 通过服务名解析地址调用订单服务order

--- shop-order-server/src/main/java/com/example/server/ShopOrderServceApplication.java
+++ shop-order-server/src/main/java/com/example/server/ShopOrderServceApplication.java
@@ -3,11 +3,13 @@ package com.example.server;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @MapperScan("com.example.server.mapper")
+@EnableDiscoveryClient
 public class ShopOrderServceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShopOrderServceApplication.class, args);
--- shop-order-server/src/main/java/com/example/server/service/impl/OrderServiceImpl.java
+++ shop-order-server/src/main/java/com/example/server/service/impl/OrderServiceImpl.java
@@ -6,8 +6,12 @@ import com.example.server.mapper.OrderMapper;
 import com.example.server.service.OrderService;
 import org.example.pojo.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 
 @Service
@@ -16,11 +20,20 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @Override
     public Order createOrder(Long pid, Long uid) {
         Order order = new Order();
         order.setPid(pid);
-        String url  = "http://localhost:8081/product/" + pid;
+//        String url  = "http://localhost:8081/product/" + pid;
+//        Product product = restTemplate.getForObject(url, Product.class);
+        List<ServiceInstance> instances = discoveryClient.getInstances("product-service");
+        ServiceInstance serviceInstance = instances.get(0);
+        String host = serviceInstance.getHost();
+        int port = serviceInstance.getPort();
+        String url = "http://" + host + ":" + port + "/product/" + pid;
         Product product = restTemplate.getForObject(url, Product.class);
         order.setProductName(product.getName());
         order.setProductPrice(product.getPrice());
